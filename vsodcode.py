df18319ad29d12338e449e69fc0a1fa5551cb1ba
crookiee <jeremytaann@gmail.com>
2023-01-04 01:08:31 +0800

    should be working

--- vsodcode.py
+++ vsodcode.py
@@ -3,16 +3,16 @@ from matplotlib import pyplot as plt
 import numpy as np
 import glob
 import torch
-from time import time
 
 def summarisevideo(videofile, duration):
+    
     #accessing the video
     video_v = cv2.VideoCapture(videofile)
     
     #summarised.mp4 only contains frames with big changes
     video_w = int(video_v.get(cv2.CAP_PROP_FRAME_WIDTH))
     video_h = int(video_v.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    summarised = cv2.VideoWriter('summarised.mp4', fourcc, 24, (video_w, video_h))
+    summarised = cv2.VideoWriter("D:/Jere Stuff/Code/GitHub/vsod/odresult/summarised.mp4", fourcc, duration, (video_w, video_h))
     
     u = 0 #unique frame counter
     thres = 20 #threshold for frame similarity
@@ -38,7 +38,7 @@ def summarisevideo(videofile, duration):
             else:
                 previous_f = current_f
                 
-            cv2.imshow('Unique Frame', current_f)
+            cv2.imshow("super fast unique frame slideshow", current_f)
         
         #stop program by pressing x
         if cv2.waitKey(1) & 0xff == ord('x'): #0xff because ord('x') may be different value due to numlock
@@ -57,38 +57,56 @@ def summarisevideo(videofile, duration):
 class detection:
     
     #prereqs, loading yolov5 model section
+    #initialize class for bruh object
     def __init__(self):
-        self.model = self.load_model()
-        self.classes = self.model.names
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu' #rearrange
-        print("\n\n device used: ", self.device)
+        self.model = self.load_model() #loads model we want to use
+        self.classes = self.model.names #get labels for object detection
         
+        if torch.cuda.is_available():
+            self.device = 'cuda' 
+            
+        else:
+            self.device = 'cpu' 
+        
+        print("\ndevice used: ", self.device)
+        
+    #load the model (tried using local path could not access, using torch.hub instead)
     def load_model(self):
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained = True)
+        model = torch.hub.load("ultralytics/yolov5", 'yolov5s', pretrained = True)
         return model
     
     #labelling and object detection section
-    def getscore(self, od_frame):
-        self.model.to(self.device)
-        od_frame = [od_frame]
-        od_score = self.model(od_frame)
-        labels, coordinates = od_score.xyxyn[0][:, -1], od_score.xyxyn[0][:, :-1]
+    def getstats(self, od_frame):
+        
+        self.model.to(self.device) #specify using cpu or gpu for the model
+        od_frame = [od_frame] #format to pass model
+        od_stats = self.model(od_frame)
+        
+        #all boundary boxes (as coordinates) and labels stored in od_stats
+        labels, coordinates = od_stats.xyxyn[0][:, -1], od_stats.xyxyn[0][:, :-1]
             
         return labels,coordinates
-        
+    
+    #for a label value, return the label as string
     def classname(self, x):
-        return self.classes[int(x)] 
-            
-    def drawbox(self, od_score, od_frame):
-        labels, coordinates = od_score
-        n = len(labels)
+        return self.classes[int(x)] #label index
+
+    def getclassname(self):
+        return self.classes
+    
+    #take frame and od_stats, and draws the boxes and labels them
+    def drawbox(self, od_stats, od_frame):
+        
+        labels, coordinates = od_stats
+        n = len(labels) #n = number of a specific label eg. 10 people
         od_w, od_h = od_frame.shape[1], od_frame.shape[0]
             
         for i in range(n):
             row = coordinates[i]
                 
             if row[4] >= 0.2:
-                    
+                
+                #widths and heights of a rectangle, od_w and od_h from our original frame
                 w1 = int(row[0] * od_w) 
                 h1 = int(row[1] * od_h) 
                 w2 = int(row[2] * od_w) 
@@ -98,39 +116,40 @@ class detection:
             cv2.rectangle(od_frame, (w1, h1), (w2, h2), bgr, 2)
             cv2.putText(od_frame, self.classname(labels[i]), (w1, h1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
                     
-        return od_frame
+        return od_frame #returns frame with the labels and boxes drawn
         
     #will get called when instance of object created
     def __call__(self):
+        
+        print("processing...")
         #accessing summarised video
-        od_video = cv2.VideoCapture("summarised.mp4")
+        od_video = cv2.VideoCapture("D:/Jere Stuff/Code/GitHub/vsod/odresult/summarised.mp4")
         
         #writing new video with object detection
         od_w = int(od_video.get(cv2.CAP_PROP_FRAME_WIDTH))
         od_h = int(od_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        od_summarised = cv2.VideoWriter("od_summarised.mp4", fourcc, 24, (od_w, od_h))
+        od_summarised = cv2.VideoWriter( "D:/Jere Stuff/Code/GitHub/vsod/odresult/od_summarised.mp4", fourcc, duration, (od_w, od_h))
         
         while True:
-            #start = time() #start time
             status_od, od_frame = od_video.read()
             if not status_od:
                 break
             
-            od_score = self.getscore(od_frame)
-            od_frame = self.drawbox(od_score, od_frame)
-            #end = time() end time
+            od_stats = self.getstats(od_frame)
+            od_frame = self.drawbox(od_stats, od_frame)
             
-            #fps = 1/np.round(end - start, 3)
-            #print(f"fps: {fps}")
+            print("...")
             od_summarised.write(od_frame)
-  
+        
+        print("processing done")
+        
 #main
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') #used in summarisevideo() and detect(), MPEG-4 codec just cause
 
-
 #passing cctv.mp4 into summarisevideo()
 videofile = "D:/Jere Stuff/Code/GitHub/vsod/cctv.mp4"
-frameslist = summarisevideo(videofile, 10)
+duration = 24 #frames per second for result video
+frameslist = summarisevideo(videofile, duration)
 
 #visualise the first 20 frames
 fig = plt.figure()
@@ -147,3 +166,12 @@ plt.show()
 #pass summarised.mp4 into detect()
 bruh = detection()
 bruh()
+
+#list of detected objects
+detected_objects = bruh.getclassname()
+print("\nlist of detected objects: \n")
+        
+for i in (detected_objects):
+    print(detected_objects[i], end = ',')
+    
+
